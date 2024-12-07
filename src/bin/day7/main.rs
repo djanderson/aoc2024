2ddb031685ed3d38b238748b267e6d7fe2e2ff05
Douglas Anderson <doug@ursamajor.com>
2024-12-07 10:34:07 -0700

    Add solution for part 2, day 7

--- src/bin/day7/main.rs
+++ src/bin/day7/main.rs
@@ -10,12 +10,13 @@ fn main() {
 fn part1() -> usize {
     let input = fs::read_to_string("input.txt").unwrap();
     let calibrations = input.lines().map(|s| Calibration::from_str(s).unwrap());
-    calibrations.map(|cal| cal.sum_valid()).sum()
+    calibrations.map(|cal| cal.sum_valid_p1()).sum()
 }
 
-fn part2() -> u32 {
+fn part2() -> usize {
     let input = fs::read_to_string("input.txt").unwrap();
-    input.lines().count() as u32
+    let calibrations = input.lines().map(|s| Calibration::from_str(s).unwrap());
+    calibrations.map(|cal| cal.sum_valid_p2()).sum()
 }
 
 #[derive(Debug)]
@@ -26,13 +27,7 @@ struct Calibration {
 
 impl Calibration {
     // Return true if value and operands could form a valid equation.
-    fn sum_valid(&self) -> usize {
-        let mut total = 0;
-        // 2 numbers = 1 bit
-        // 3 numbers = 2 bits
-        // 4 numbers = 3 bits
-        // etc...
-        //
+    fn sum_valid_p1(&self) -> usize {
         // The approach here is to use the bits of a range of numbers to
         // pick the operator between any two numbers.
         // For example, if I have the Calibration 3267: 81 40 27, I
@@ -51,6 +46,13 @@ impl Calibration {
         // Instead of walking the bits, I just look at the low bit (with & 1) and
         // right shift the number to move the next highest bit into that position
         // on each iteration.
+        //
+        // The number of bits I need is:
+        // 2 numbers = 1 bit
+        // 3 numbers = 2 bits
+        // 4 numbers = 3 bits
+        // etc...
+        let mut total = 0;
         let nbits = self.operands.len() as u32 - 1;
         for i in 0..(2u32.pow(nbits)) {
             let mut n = i;
@@ -66,6 +68,44 @@ impl Calibration {
                     break;
                 }
                 n = n >> 1;
+            }
+            if value == self.target_value {
+                total += value;
+                break; // only add the number once if it's valid
+            }
+        }
+        total
+    }
+
+    fn sum_valid_p2(&self) -> usize {
+        // The approach here is very similar to sum_valid_p1 but
+        // we're using ternary (base 3) math instead of base 2,
+        // which means we don't get to play the same trick of using
+        // incrementing numbers to create our bit pattern. Instead
+        // I create an enum with 3 valid states, and implement an iterator
+        // over the "trits" (ternary bits) until all perumutations are
+        // exhausted.
+        let mut total = 0;
+        let ntrits = self.operands.len() - 1;
+        let mut operations = OpPerumutations::new(ntrits);
+        'outer: loop {
+            let mut operands = self.operands.iter().copied();
+            let mut value = operands.next().unwrap();
+            for operand in operands {
+                let Some(operation) = operations.next() else {
+                    break 'outer;
+                };
+                let next_value = match operation {
+                    Operator::Add => value + operand,
+                    Operator::Multiply => value * operand,
+                    Operator::Concat => {
+                        let mut s = value.to_string();
+                        s.push_str(&operand.to_string());
+                        s.parse().expect("concatted number")
+                    }
+                };
+                //println!("{value} {operation:#?} {operand} = {next_value}");
+                value = next_value;
             }
             if value == self.target_value {
                 total += value;
@@ -94,5 +134,60 @@ impl FromStr for Calibration {
             target_value: value,
             operands,
         })
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Operator {
+    Add,
+    Multiply,
+    Concat,
+}
+
+struct OpPerumutations {
+    ops: Vec<Operator>,
+    cur: usize,
+    max_cur: usize,
+    carry: bool,
+}
+
+impl OpPerumutations {
+    fn new(n: usize) -> Self {
+        Self {
+            ops: vec![Operator::Add; n],
+            cur: 0,
+            max_cur: 3usize.pow(n as u32) * n,
+            carry: false,
+        }
+    }
+}
+
+impl Iterator for OpPerumutations {
+    type Item = Operator;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur == self.max_cur {
+            return None;
+        }
+        let cur = self.cur % self.ops.len();
+        let op = self.ops[cur];
+        if cur == 0 || self.carry {
+            self.carry = match op {
+                Operator::Add => {
+                    self.ops[cur] = Operator::Multiply;
+                    false
+                }
+                Operator::Multiply => {
+                    self.ops[cur] = Operator::Concat;
+                    false
+                }
+                Operator::Concat => {
+                    self.ops[cur] = Operator::Add;
+                    true
+                }
+            };
+        }
+        self.cur += 1;
+        Some(op)
     }
 }
